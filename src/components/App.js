fa378e50683d3e10e9c42e502f7f99e9232dffa2
ADARSH SHARMA <120009672+Adarshsharma42876@users.noreply.github.com>
2023-04-23 15:05:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,70 +1,65 @@
-import React, { useState, useEffect } from "react";
-import "../styles/App.css";
+import React, { useState, useEffect } from 'react';
+import "../styles/App.css"
 
-const WORD_LIST = ["apple", "banana", "cherry", "grape", "orange"];
+const WORD_LIST = ['apple', 'banana', 'cherry', 'grape', 'orange'];
 
 function App() {
-  const [word, setWord] = useState("");
+  const [word, setWord] = useState('');
   const [flashWord, setFlashWord] = useState(true);
-  const [userInput, setUserInput] = useState("");
-  const [result, setResult] = useState("");
+  const [userInput, setUserInput] = useState('');
+  const [result, setResult] = useState('');
   const [index, setIndex] = useState(0);
-  useEffect(() => {
-    if (flashWord) {
-      setWord(WORD_LIST[index]);
-    }
-    const timer = setTimeout(() => {
-      setWord("");
 
-      setFlashWord(!true);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFlashWord(false);
     }, 500);
+
+    return () => clearTimeout(timeout);
+  }, [word]);
+
+  useEffect(() => {
+    setWord(WORD_LIST[index]);
+    setFlashWord(true);
+    setUserInput('');
+    setResult('');
   }, [index]);
 
-  const handleInputChange = (e) => {
-    setUserInput(e.target.value);
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value);
   };
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    if (WORD_LIST.includes(userInput.toLowerCase())) {
-      setResult("You won!");
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (userInput.toLowerCase() === word.toLowerCase()) {
+      setResult('You won!');
     } else {
-      setResult("You lost!");
+      setResult('You lost!');
     }
-    setUserInput("");
   };
+
   const handleRestartClick = () => {
-    setResult("");
-    setFlashWord(true);
-    setIndex((pre) => pre + 1);
+    if (index === WORD_LIST.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
   };
+
   return (
     <div className="mini-game-container">
       <h2 className="mini-game-title">Mini Game</h2>
-      <p className="mini-game-word">{word}</p>
-      {result.length ? (
-        ""
-      ) : (
+      {flashWord && <p className="mini-game-word">{word}</p>}
+      {!flashWord && (
         <form className="mini-game-form" onSubmit={handleFormSubmit}>
-          <input
-            className="mini-game-input"
-            type="text"
-            value={userInput}
-            onChange={handleInputChange}
-          />
-          <button className="mini-game-button" type="submit">
-            Check Answer
-          </button>
+          <input className="mini-game-input" type="text" value={userInput} onChange={handleInputChange} />
+          <button className="mini-game-button" type="submit">Check Answer</button>
         </form>
       )}
       {result && (
         <>
           <p className="mini-game-result">{result}</p>
-          <button
-            className="mini-game-restart-button"
-            onClick={handleRestartClick}
-          >
-            Restart
-          </button>
+          <button className="mini-game-restart-button" onClick={handleRestartClick}>Restart</button>
         </>
       )}
     </div>
